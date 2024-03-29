8fda5b504ab6726cb7b92741dcfd2c87787880a5
tahasama <taha.maatof@gmail.com>
2024-03-29 12:21:30 +0000

    install + quick response

--- codeConverter/views.py
+++ codeConverter/views.py
@@ -7,9 +7,12 @@ from matplotlib import pyplot as plt
 from django.http import JsonResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 
-
+# Import other necessary modules and clear Keras session
 from keras import backend as K
 K.clear_session()
+
+# Initialize a dictionary to cache installed packages
+installed_packages = {}
 
 @csrf_exempt
 def execute_python(request):
@@ -20,20 +23,17 @@ def execute_python(request):
     code = data.get('code', '')
     required_packages = data.get('requiredPackages', [])
 
-    # Install required packages
+    # Install required packages if not already installed
     for package in required_packages:
-        subprocess.run(['pip', 'install', package])
+        if package not in installed_packages:
+            subprocess.run(['pip', 'install', package])
+            installed_packages[package] = True
 
     # Check if the code contains any plotting commands
-    has_plots = False
-    for line in code.split('\n'):
-        if ('plt.' in line and not '# plt.' in line) or ('sns.' in line and not '# sns.' in line):
-            has_plots = True
-            break
-    for line in code.split('\n'):
-        if ' input'+'(' in line:
-            result = "Sorry, the 'input' command is not supported. if you have a variable with name 'input' please change it"
-            return JsonResponse({'result': result, 'result_images': [], 'error': ''})
+    has_plots = any(('plt.' in line and not '# plt.' in line) or ('sns.' in line and not '# sns.' in line) for line in code.split('\n'))
+    if 'input(' in code:
+        result = "Sorry, the 'input' command is not supported. If you have a variable with name 'input', please change it."
+        return JsonResponse({'result': result, 'result_images': [], 'error': ''})
 
     # Redirect stdout and stderr to string buffers
     stdout_buffer = io.StringIO()
@@ -58,7 +58,7 @@ def execute_python(request):
                     plt.figure(fig).savefig(img_data, format='png', bbox_inches='tight')
                     encoded_img = base64.b64encode(img_data.getvalue()).decode('utf-8')
                     images.append(f"data:image/png;base64,{encoded_img}")
-            plt.clf() # Clear the current figure after saving all images
+            plt.clf()  # Clear the current figure after saving all images
 
             # Collect stdout output
             result = stdout_buffer.getvalue().strip()
